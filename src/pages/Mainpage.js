403743710a756df94b8540ffaa3f87dd8f6da337
james-hardy <topsec115@gmail.com>
2021-05-10 06:35:13 -0400

    added icons

--- src/pages/Mainpage.js
+++ src/pages/Mainpage.js
@@ -2,8 +2,13 @@ import React from 'react';
 import Textbox from '../components/Textbox';
 import { Parallax } from 'react-parallax';
 import { Container, Row, Col } from 'reactstrap';
-import mac from '../assets/images/IMG_1531.jpeg'
+import Typography from "@material-ui/core/Typography";
 import './style.css'
+import Projects from '../components/Projects';
+import mac from '../assets/images/IMG_1531.jpeg';
+import coffee from '../assets/images/IMG_1535.jpeg';
+import gr from '../assets/images/gr2.jpeg';
+
 
 
 
@@ -11,21 +16,58 @@ import './style.css'
 const Mainpage = () => (
 
     <div>
-    <Parallax bgImage={mac} alt = 'pic' strength={200}>
+    <Parallax bgImage={mac} alt = 'mac pic' strength={200}>
         <div className="flexitem">
-            <h3>icon one</h3>
-            <h3>icon two</h3>
-            <h3>icon three</h3>
+            <i class="fas fa-home"></i>
+            <i class="fas fa-th"></i>
+            <i class="far fa-user"></i>
+            <i class="far fa-envelope"></i>
         </div>
         <div className="image">
-            <div className="macpxtext">Hello</div>
+        <Typography
+              component="h4"
+              variant="h5"
+              align="left"
+              color="textSecondary"
+              gutterBottom
+            >
+              Hello, My name is
+            </Typography>
+       
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Hector Diaz Jr
+            </Typography>
+            <Typography
+              variant="h5"
+              align="right"
+              color="textSecondary"
+              paragraph
+            >
+              I am a Full Web Stack Developer!
+            </Typography>
         </div>
 
     </Parallax>
     <Textbox>
         projects components here
     </Textbox>
-    <Parallax bgImage='https://info.thinkfun.com/hubfs/coding%201.jpg' strength={500}>
+    <Parallax bgImage={coffee} alt = 'coffe pic' strength={200}>
+        <div style={{height: 600}}>
+
+        </div>
+
+    </Parallax>
+    <Textbox>
+        <Projects />
+    </Textbox>
+
+    <Parallax bgImage={gr} alt = 'GR pic' strength={200}>
         <div style={{height: 600}}>
 
         </div>
@@ -33,6 +75,7 @@ const Mainpage = () => (
     </Parallax>
     <Textbox>
         about me component
+        <img className="myPix" src="../assets/images/hd.jpeg"></img>
     </Textbox>
     </div>
 
@@ -42,3 +85,11 @@ const Mainpage = () => (
 );
 
 export default Mainpage
+
+
+//github icon
+//<i class="fab fa-github"></i>
+
+//linkedin icon
+//<i class="fab fa-linkedin"></i>
+
